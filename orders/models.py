2a0a7e7588972de12724bb07cea3a2c3e8e883f4
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-15 18:38:50 +0530

    63772

--- orders/models.py
+++ orders/models.py
@@ -15,3 +15,13 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    discount_percentage = models.DecimalField(max_digits=4, decimal_places=2)
+    is_active = models.BooleanField()
+    valid_from = models.DateField()
+    valid_until = models.DateField()
+
+    def __str__(self):
+        return self.code

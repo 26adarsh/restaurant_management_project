ebb9a327192ef3fc656c8fbcd05d030c364a983c
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-23 15:11:32 +0530

    64615

--- home/models.py
+++ home/models.py
@@ -39,3 +39,17 @@ class NutritionalInformation(models.Model):
 
     def __str__(self):
         return f"{self.menu_item.name}-{self.calories}kcal"
+
+class Ingredient(models.Model):
+    name=models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
+    ingredients = models.ManyToManyField(Ingredient,related_name="menu_items")
+
+    def __str__(self):
+        return self.name

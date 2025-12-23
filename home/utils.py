82f25badf5259d9b385f40afa4708118cd0eb7be
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-23 15:29:56 +0530

    64616

--- home/utils.py
+++ home/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime,time
+import re
 
 def is_restaurant_open():
     now = datetime.now()
@@ -12,3 +13,7 @@ def is_restaurant_open():
         open_time=time(10,0)
         close_time=time(23,0)
     return open_time <= current_time<=close_time
+
+def is_valid_phone_number(phone_number:str)->bool:
+    pattern = r'^(\+\d{1,3}[\s-]?)?\d{3,4}[\s-]?\d{3,4}[\s-]?\d{3,4}$'
+    return bool(re.match(pattern,phone_number))

ecabd1d6f156d9387b2c8109eb0f980fbb7e8a89
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-23 15:25:30 +0530

    64615

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import MenuCategoryListView
+from .views import MenuCategoryListView,MenuItemIngredientsView
 
 urlpatterns = [
     path('menu-categories/',MenuCategoryListView.as_view()),
     path('menu/featured/',featuredMenuItemListView.as_view(),name='featured-menu-items'),
+    path('api/menu-items/<int:pk>/ingredients/',MenuItemIngredientsView.as_view(),
+    name='menu-item-ingredients'),
 ]

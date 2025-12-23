b0ea4b2b347bc57e13de675b5d5c48e102644948
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-23 15:24:09 +0530

    64615

--- home/views.py
+++ home/views.py
@@ -5,6 +5,11 @@ from .serializers import MenuCategorySerializer,MenuItemSerializer
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.pagination import PageNumberPagination
 from .models import MentItem
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response 
+from rest_framework import status
+from .models import MentItem
+from .serializers import IngredientSerializer
 
 # Create your views here.
 
@@ -32,3 +37,12 @@ class MenuItemSearchViewSet(ReadOnlyModelViewSet):
             queryset=queryset.filter(name__icontains=query)
 
         return queryset
+    
+class MentItemIngredientsView(RetrieveAPIView):
+    queryset=MentItem.objects.all()
+
+    def retrieve(self,request,*args,**kwargs):
+        menu_item=self.get_object()
+        ingredients=menu_item.ingredients.all()
+        serializer=IngredientSerializer(ingredients,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)

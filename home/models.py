5c5eb79a6ffdaf71874583414356f73cd0b72693
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-23 15:47:51 +0530

    64637

--- home/models.py
+++ home/models.py
@@ -48,9 +48,11 @@ class Ingredient(models.Model):
         return self.name
 
 class MenuItem(models.Model):
+    category=models.ForeignKey(MenuCategory,on_delete=models.CASCADE,related_name='items')
     name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
     price=models.DecimalField(max_digits=8,decimal_places=2)
-    ingredients = models.ManyToManyField(Ingredient,related_name="menu_items")
+    is_available=models.BooleanField(default=True)
 
     def __str__(self):
         return self.name

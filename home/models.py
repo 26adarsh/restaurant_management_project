b21bf464473149299238d140b87c922760f8061b
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2025-12-23 13:21:32 +0530

    64610

--- home/models.py
+++ home/models.py
@@ -8,3 +8,19 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class DailySpecial(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=8,decimal_places=2)
+    is_available=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    @staticmethod
+    def get_random_special():
+        specials=DailySpecial.objects.filter(is_available=True)
+        if not specials.exists():
+            return None
+        return specials.order_by('?').first()

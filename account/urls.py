0adf3b231b24b92e319816785efb70fb5339e136
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2026-01-02 13:04:12 +0530

    65280

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+user_profile=UserProfileViewSet.as_view({'put':'update'})
+
 urlpatterns = [
-    
+    path('profile/',user_profile,name='user-profile-update'),
 ]

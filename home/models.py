9bd7688a6a650a54c20fd13986cff28984fbfebe
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2026-01-02 14:12:06 +0530

    65281

--- home/models.py
+++ home/models.py
@@ -80,3 +80,22 @@ class OrderItem(models.Model):
         related_name='orderitem'
     )
     quantity = models.PositiveIntegerField(default=1)
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    owner_name = models.CharField(max_length=255)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=20)
+    address=models.TextField()
+    city=models.CharField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    operating_days=models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text="Comma-seprated days"
+    )
+    def __str__(self):
+        return self.name

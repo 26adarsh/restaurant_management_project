efa712f6f6f11fb62ccd23011e3f6d5b4739b70f
Adarsh Mishra <121840972+26adarsh@users.noreply.github.com>
2026-01-02 13:03:19 +0530

    65280

--- account/views.py
+++ account/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ViewSet 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserProfileSerializer
+
+class UserProfileViewSet(ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def update(self,request):
+        serializer = UserProfileSerializer(
+            request.user,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.
